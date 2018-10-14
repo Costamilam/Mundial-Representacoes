4137133b5424e47c69442cb24084f925cf61a5b8
Guilherme Costamilam <guilherme@Inspiron.Costamilam>
2018-10-14 20:22:52 -0300

    Add carousel and cross-browser

--- index.php
+++ index.php
@@ -5,18 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" href="./images/globe.ico">
+    <link rel="icon" href="./images/icon_globe.ico">
 
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-red.min.css" />
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700">
+    <link type="text/css" rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+    <link type="text/css" rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-red.min.css" />
+    <script defer type="text/javascript" src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 
-    <script defer src="https://swc.cdn.skype.com/sdk/v1/sdk.min.js"></script>
+    <script defer type="text/javascript" src="https://swc.cdn.skype.com/sdk/v1/sdk.min.js"></script>
 
     <link type="text/css" rel="stylesheet" href="./style.css">
     <link type="text/css" rel="stylesheet" href="./animate.css">
+
+    <script defer type="text/javascript" src="./main.js"></script>
+
     <?php
         require './vendor/autoload.php';
 
@@ -26,7 +28,7 @@
         if ($_SERVER["REQUEST_METHOD"] === "POST") {
             $mail = new PHPMailer();
             $mail->isSMTP();
-            $mail->Host = ''; 
+            $mail->Host = '';
             $mail->SMTPAuth = true;
             $mail->Username = '';
             $mail->Password = '';
@@ -71,12 +73,12 @@
             <div id="layout-padding" class="mdl-cell mdl-cell--8-col-desktop mdl-cell--2-offset-desktop mdl-cell--12-col-tablet mdl-cell--0-offset-tablet">
                 <article class="fadeInUp delay-1s">
                     <figure style="background-color: transparent; max-height: none;" class="mdl-card__media">
-                        <img style="max-height: none;" class="article-image" src="images/logo_transparente.png" alt="Logo Mundial Representações">
+                        <img style="max-height: none;" class="article-image" src="./images/logo_mundial.png" alt="Logo Mundial Representações">
                     </figure>
                 </article>
 
                 <article class="fadeInUp delay-2s mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp">
-                    <hgroup style="margin: 0 auto;" class="mdl-card__title">
+                    <hgroup class="mdl-card__title">
                         <h2 class="mdl-card__title-text">Quem Somos</h2>
                     </hgroup>
                     <section class="mdl-card__supporting-text">
@@ -85,7 +87,7 @@
                 </article>
 
                 <article id="mvv" class="mdl-cell mdl-cell--12-col mdl-grid">
-                    <section class="fadeInUp delay-3s mdl-cell mdl-cell--4-col-desktop mdl-cell--12-col-tablet mdl-card mdl-shadow--4dp">
+                    <section class="fadeInUp delay-3-5s mdl-cell mdl-cell--4-col-desktop mdl-cell--12-col-tablet mdl-card mdl-shadow--4dp">
                         <hgroup class="mdl-card__title">
                             <h2 class="mdl-card__title-text">Missão</h2>
                         </hgroup>
@@ -94,7 +96,7 @@
                         </section>
                     </section>
             
-                    <section class="fadeInUp delay-3s mdl-cell  mdl-cell--4-col-desktop mdl-cell--12-col-tablet mdl-card mdl-shadow--4dp">
+                    <section class="fadeInUp delay-3s mdl-cell mdl-cell--4-col-desktop mdl-cell--12-col-tablet mdl-card mdl-shadow--4dp">
                         <hgroup class="mdl-card__title">
                             <h2 class="mdl-card__title-text">Visão</h2>
                         </hgroup>
@@ -103,7 +105,7 @@
                         </section>
                     </section>
             
-                    <section class="fadeInUp delay-3s mdl-cell mdl-cell--4-col-desktop mdl-cell--12-col-tablet mdl-card mdl-shadow--4dp">
+                    <section class="fadeInUp delay-3-5s mdl-cell mdl-cell--4-col-desktop mdl-cell--12-col-tablet mdl-card mdl-shadow--4dp">
                         <hgroup class="mdl-card__title">
                             <h2 class="mdl-card__title-text">Valores</h2>
                         </hgroup>
@@ -113,11 +115,32 @@
                     </section>
                 </article>
 
+                <article id="carousel" class="fadeInUp delay-4s mdl-cell mdl-cell--12-col mdl-grid">
+                    <section>
+                        <figure>
+                            <img class="mdl-shadow--4dp" src="./images/product_merco.png" alt="Merco Parts">
+                            <img class="mdl-shadow--4dp" src="./images/product_notus.png" alt="Notus">
+                            <img class="mdl-shadow--4dp" src="./images/product_yiming.png" alt="Yiming Parts">
+                            <img class="mdl-shadow--4dp" src="./images/product_metan.png" alt="Metan">
+                            <img class="mdl-shadow--4dp" src="./images/product_brunko.png" alt="Brunko">
+                            <img class="mdl-shadow--4dp" src="./images/product_ndflex.png" alt="NDflex">
+                        </figure>
+                    </section>
+                    <section>
+                        <button id="previous-button" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                            <i class="fas fa-arrow-circle-left"></i>
+                        </button>
+                        <button id="next-button" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                            <i class="fas fa-arrow-circle-right"></i>
+                        </button>
+                    </section>
+                </article>
+
                 <article class="mdl-grid mdl-cell mdl-cell--12-col">
-                    <section class="fadeInUp delay-4s mdl-cell mdl-cell--6-col-desktop mdl-cell--12-col-tablet mdl-card mdl-shadow--4dp">
+                    <section class="fadeInUp delay-5s mdl-cell mdl-cell--6-col-desktop mdl-cell--12-col-tablet mdl-card mdl-shadow--4dp">
                         <figure class="mdl-card__media">
                             <a href="http://mercopartsjuntas.com.br" title="mercopartsjuntas.com.br" target="_blank">
-                                <img class="article-image" src="./images/merco.png" border="0" alt="">
+                                <img class="article-image" src="./images/logo_merco.png" border="0" alt="">
                             </a>
                         </figure>
                         <section class="mdl-card__supporting-text">
@@ -128,10 +151,10 @@
                         </section>
                     </section>
 
-                    <section class="fadeInUp delay-4s mdl-cell mdl-cell--6-col-desktop mdl-cell--12-col-tablet mdl-card mdl-shadow--4dp">
+                    <section class="fadeInUp delay-5s mdl-cell mdl-cell--6-col-desktop mdl-cell--12-col-tablet mdl-card mdl-shadow--4dp">
                         <figure class="mdl-card__media">
                             <a href="http://yiming.com.br" title="yiming.com.br" target="_blank">
-                                <img class="article-image" src="./images/yiming.jpeg" border="0" alt="">
+                                <img class="article-image" src="./images/logo_yiming.jpeg" border="0" alt="">
                             </a>
                         </figure>
                         <section class="mdl-card__supporting-text">
@@ -142,10 +165,10 @@
                         </section>
                     </section>
 
-                    <section class="fadeInUp delay-4-2s mdl-cell mdl-cell--6-col-desktop mdl-cell--12-col-tablet mdl-card mdl-shadow--4dp">
+                    <section class="fadeInUp delay-5-2s mdl-cell mdl-cell--6-col-desktop mdl-cell--12-col-tablet mdl-card mdl-shadow--4dp">
                         <figure class="mdl-card__media">
                             <a href="http://notus.ind.br" title="notus.ind.br" target="_blank">
-                                <img class="article-image" src="./images/notus.png" border="0" alt="">
+                                <img class="article-image" src="./images/logo_notus.png" border="0" alt="">
                             </a>
                         </figure>
                         <section class="mdl-card__supporting-text">
@@ -157,9 +180,9 @@
                         </section>
                     </section>
 
-                    <section class="fadeInUp delay-4-2s mdl-cell mdl-cell--6-col-desktop mdl-cell--12-col-tablet mdl-card mdl-shadow--4dp">
+                    <section class="fadeInUp delay-5-2s mdl-cell mdl-cell--6-col-desktop mdl-cell--12-col-tablet mdl-card mdl-shadow--4dp">
                         <figure class="mdl-card__media">
-                            <img class="article-image" src="./images/metan.png" border="0" alt="">
+                            <img class="article-image" src="./images/logo_metan.png" border="0" alt="">
                         </figure>
                         <section class="mdl-card__supporting-text">
                             A Metan trabalha a mais de 40 anos com solida experiência no mercado de peças de reposição, na linha de cabeçotes Volkswagem.
@@ -169,9 +192,9 @@
                         </section>
                     </section>
 
-                    <section class="fadeInUp delay-4-4s mdl-cell mdl-cell--6-col-desktop mdl-cell--12-col-tablet mdl-card mdl-shadow--4dp">
+                    <section class="fadeInUp delay-5-4s mdl-cell mdl-cell--6-col-desktop mdl-cell--12-col-tablet mdl-card mdl-shadow--4dp">
                         <figure class="mdl-card__media">
-                            <img class="article-image" src="./images/brunko.jpeg" border="0" alt="">
+                            <img class="article-image" src="./images/logo_brunko.jpeg" border="0" alt="">
                         </figure>
                         <section class="mdl-card__supporting-text">
                             A Brunko é fabricante de blocos e cabeçotes Mercedes, sendo referência em qualidade e atendimento. Possui infra-estrutura precisa e o conhecimento das necessidades de seus consumidores para atender e superar suas expectativas.
@@ -181,10 +204,10 @@
                         </section>
                     </section>
 
-                    <section class="fadeInUp delay-4-4s mdl-cell mdl-cell--6-col-desktop mdl-cell--12-col-tablet mdl-card mdl-shadow--4dp">
+                    <section class="fadeInUp delay-5-4s mdl-cell mdl-cell--6-col-desktop mdl-cell--12-col-tablet mdl-card mdl-shadow--4dp">
                         <figure class="mdl-card__media">
                             <a href="http://ndflex.com.br" title="ndflex.com.br" target="_blank">
-                                <img class="article-image" src="./images/ndflex.jpeg" border="0" alt="">
+                                <img class="article-image" src="./images/logo_ndflex.jpeg" border="0" alt="">
                             </a>
                         </figure>
                         <section class="mdl-card__supporting-text">
@@ -195,9 +218,9 @@
                         </section>
                     </section>
 
-                    <section class="fadeInUp delay-4-6s mdl-cell mdl-cell--6-col-desktop mdl-cell--3-offset-desktop mdl-cell--12-col-tablet mdl-card mdl-shadow--4dp">
+                    <section class="fadeInUp delay-5-6s mdl-cell mdl-cell--6-col-desktop mdl-cell--3-offset-desktop mdl-cell--12-col-tablet mdl-card mdl-shadow--4dp">
                         <figure class="mdl-card__media">
-                            <img class="article-image" src="./images/fortmetal.png" border="0" alt="">
+                            <img class="article-image" src="./images/logo_fortmetal.png" border="0" alt="">
                         </figure>
                         <section class="mdl-card__supporting-text">
                             A FortMetal fabrica bandejas de suspensão dentro dos mais rígidos padrões de qualidade e com materiais de primeira linha.
@@ -208,7 +231,7 @@
                     </section>
                 </article>
 
-                <article id="contact" class="fadeInUp delay-5s mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp">
+                <article id="contact" class="fadeInUp delay-6s mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp">
                     <hgroup style="margin: 0 auto;" class="mdl-card__title">
                         <h2 class="mdl-card__title-text">Contato</h2>
                     </hgroup>
@@ -216,48 +239,48 @@
                         <p>Solicite uma cotação ou tire suas dúvidas, será um prazer atende-lo.</p>
 
                         <form action="./" method="POST">
-                            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <section class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                                 <input class="mdl-textfield__input" type="text" id="name" name="name" required pattern="^[^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\],.]{2,}$">
                                 <label class="mdl-textfield__label" for="name">Nome *</label>
-                            </div>
+                            </section>
 
-                            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <section class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                                 <input class="mdl-textfield__input" type="email" id="email" name="email">
                                 <label class="mdl-textfield__label" for="email">Email</label>
-                            </div>
+                            </section>
 
-                            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <section class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                                 <input class="mdl-textfield__input" type="tel" id="phone" name="phone" required pattern="^\(?(\d{2})?\)?[\s-]?9?\d{4}[ \.\-]?\d{4}$">
                                 <label class="mdl-textfield__label" for="phone">Telefone *</label>
-                            </div>
+                            </section>
 
-                            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <section class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                                 <input class="mdl-textfield__input" type="text" id="city" name="city" required pattern="^[^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\],.]{2,}$">
                                 <label class="mdl-textfield__label" for="city">Cidade *</label>
-                            </div>
+                            </section>
 
-                            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <section class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                                 <textarea class="mdl-textfield__input" type="text" rows="5" id="message" name="message" required minlength="10"></textarea>
                                 <label class="mdl-textfield__label" for="message">Mensagem *</label>
-                            </div>
+                            </section>
 
                             <button class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Enviar Mensagem</button>
                         </form>
                     </section>
                 </article>
 
-                <article class="fadeInUp mdl-cell mdl-cell--12-col mdl-card delay-6s">
+                <article class="fadeInUp delay-7s mdl-cell mdl-cell--12-col mdl-card">
                     <figure>
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d863.7100792308112!2d-51.165210579522686!3d-30.012741195612332!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x658bd70047944ff7!2sGiovani+Paiva+Costamilan+Cia!5e0!3m2!1spt-BR!2sbr!4v1530468577194" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
                     </figure>
                 </article>
             </div>
 
-            <footer class="mdl-mega-footer mdl-cell--12-col">
+            <footer class="mdl-mega-footer">
                 <article class="mdl-mega-footer__top-section">
                     <section class="mdl-mega-footer__left-section ">
-                        <a title="Página no Facebook" href="https://www.facebook.com/mundial.representacoes?hc_ref=ARSZ_HX42E256niFIY-K4n4HhQIMbymdGOTc6hzeDtjr3NjDOIuc4GUrzEQM8cpEUGg&fref=nf" target="_blank" class="fa fa-facebook mdl-mega-footer__social-btn"></a>
-                        <a title="Página no Linkedin" href="https://www.linkedin.com/company/mundial-representacoes/" target="_blank" class="fa fa-linkedin mdl-mega-footer__social-btn"></a>
+                        <a title="Página no Facebook" class="fab fa-facebook-f mdl-mega-footer__social-btn" href="https://www.facebook.com/mundial.representacoes?hc_ref=ARSZ_HX42E256niFIY-K4n4HhQIMbymdGOTc6hzeDtjr3NjDOIuc4GUrzEQM8cpEUGg&fref=nf" target="_blank"></a>
+                        <a title="Página no Linkedin" class="fab fa-linkedin-in mdl-mega-footer__social-btn" href="https://www.linkedin.com/company/mundial-representacoes/" target="_blank"></a>
                     </section>
     
                     <section style="padding-right: 50px;" class="mdl-mega-footer__right-section">
--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+const carousel = document.querySelector('#carousel > section:first-child > figure');
+let selectedIndex = 0;
+
+function rotateCarousel() {
+    carousel.style.transform = `translateZ(-390px) rotateY(${selectedIndex / 6 * -360}deg)`;
+}
+
+document.getElementById('previous-button').addEventListener('click', function() {
+    selectedIndex--;
+    rotateCarousel();
+});
+
+document.getElementById('next-button').addEventListener('click', function() {
+    selectedIndex++;
+    rotateCarousel();
+});
+
+/*
 HTMLElement.prototype.isVisible = function() {
     const position = this.getClientRects()[0];
     return position.top >= 0 && position.top <= window.innerHeight;
@@ -23,3 +41,4 @@ if (window.addEventListener) {
     window.attachEvent('onscroll', handler);
     window.attachEvent('onresize', handler);
 }
+*/
